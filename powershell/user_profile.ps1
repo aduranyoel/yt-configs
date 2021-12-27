1c287ce21742fadd0c14f22d852df3617575d702
Maday Concepcion <maday.con92@gmail.com>
2021-12-27 14:41:33 -0300

    add: README

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,10 +1,24 @@
 Import-Module oh-my-posh
 oh-my-posh --init --shell pwsh --config $env:USERPROFILE\Documents\yt-configs\powershell\yt-prompt.omp.json | Invoke-Expression
 
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Icons
+Import-Module Terminal-Icons
+
 # Alias
 Set-Alias v nvim
 Set-Alias g git
 Set-Alias ll ls
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 Set-Alias grep findstr
+Set-Alias .. cd..
 
